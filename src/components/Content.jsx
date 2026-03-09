4e7b11aa6a8d901509f7014271781e574859dfef
Harsh <harshsingh45831@gmail.com>
2026-03-09 23:54:41 +0530

    updates

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+function Content() {
+  return (
+    <div>
+      <h2>Products Page</h2>
+      <ol>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ol>
+    </div>
+  )
+}
+
+export default Content
